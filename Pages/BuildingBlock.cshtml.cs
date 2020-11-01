e91f4daab45ac254e73e6b3c1e8759652b83d269
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-11-01 03:42:03 +0200

    Implement basic building block editor

---
+++ Pages/BuildingBlock.cshtml.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SmartCityPlanner.Data;
+using SmartCityPlanner.Models;
+
+namespace SmartCityPlanner.Pages
+{
+    public class BuildingBlockModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public BuildingBlock BuildingBlock { get; set; }
+
+        public BuildingBlockModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var block = await _context.BuildingBlocks.FindAsync(id);
+            if (block == null)
+            {
+                return BadRequest();
+            }
+            return Page();
+        }
+    }
+}
